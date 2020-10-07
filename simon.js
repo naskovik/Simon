6d40f5afba32fc6da2c09bb4a38dddb02f051439
naskovik <nazarskoreyko@gmail.com>
2020-10-07 21:38:57 +0200

    Rebuild

--- simon.js
+++ simon.js
@@ -1,67 +1,43 @@
-const secrets = ['A', 'X', 'E', 'S'];
 const colors = ['rojo','amarillo','verde','azul'];
-let combinations = {};
 
-const SimonsCombination = function() {
+
+const SimonsOrder = function() {
   
   let playing;
-  let gameCombination;
-  return {
-    oninit: (vnode) => {
-      gameCombination = [...Array(4).keys()].map(() => colors[Math.floor(Math.random() * 4)]);
-      combinations.game = gameCombination.toString();
-      console.log(gameCombination);
-      playing = vnode.attrs.playing;
-    },
-    view: (vnode) => {
-      return [
-        playing ? m('.ui.segment.green', [
-          [...Array(4).keys()].map((i) => m('label', {
-            style: { marginLeft: '20px', fontSize: '25px' }
-          }, gameCombination[i])),
-        ]) : null,
-      ]
-    }
-  }
-}
+  let playersKeys;
+  let difficulty;
+  let currentColors;
 
-const PlayersCombination = function() {
-  let playing;
-  let combination;
-  return {
-    oninit: (vnode) => {
-      playing = vnode.attrs.playing;
-      combination = vnode.attrs.playersCombination;
-      combinations.player = combination.toString()
-    },
-    view: (vnode) => {
-      return [
-        playing ? m('.ui.segment.green', [
-          [...Array(4).keys()].map((i) => m('label', {
-            style: { marginLeft: '20px', fontSize: '25px' }
-          }, combination[i])),
-        ]) : null,
-        // vnode.attrs.playersCombination.length = 0,
-      ]
-    },
+  function calculateColors() {
+    return [...Array(difficulty).keys()].map(() => colors[Math.floor(Math.random() * 4)]);
   }
-}
 
-const Result = function() {
-  let playing;
+  function compareColors(arr1, arr2) {
+    return arr1.slice(-difficulty).toString() === arr2.slice(-difficulty).toString();
+  }
+
+
   return {
     oninit: (vnode) => {
+      currentColors = calculateColors();
+      difficulty = vnode.attrs.difficulty;
       playing = vnode.attrs.playing;
+      playersKeys = vnode.attrs.playersKeys;
     },
     view: () => {
       return [
-        playing ? m('.ui.container', [
-          combinations.player === combinations.game  ? 
-          m('.ui.segment.inverted.green', `${combinations.game}   VICTORIA`)
-          :
-          m('.ui.segment.inverted.red', `${combinations.game} - ${combinations.player}    DERROTA`)
-        ]) : null,
+         [...Array(difficulty).keys()].map(() => m(`.ui.segment.green`, [
+          m('label', {
+            style: { marginLeft: '20px', fontSize: '25px'}
+          }, currentColors),
+        ],
+        ))
       ]
+    },
+    onbeforeupdate: () => {
+      console.log(playersKeys.slice(- difficulty).toString() === currentColors.slice(-difficulty).toString());
+      console.log(currentColors.slice(-difficulty).toString());
+      compareColors(playersKeys, currentColors) ? currentColors = calculateColors() : null;
     },
   }
 }
@@ -69,29 +45,44 @@ const Result = function() {
 
 const Simon = function(){
   let playing;
-  let playersCombination = [];
+  let playersKeys;
   let secretShowTimeout;
-  let showSecrets;
-  let baseColor = 'DarkGrey'
+  let baseColor;
+  let difficulty;
+
+  function addColorToArray(color) {
+    playersKeys.push(color);
+  }
+  function showColors(show) { return show; }
+
   return {
-    oninit: () => {
-      showSecrets = false;
+    oninit: () => { 
+      baseColor  = 'DarkGrey';
       secretShowTimeout = 300;
       playing = false;
-      playersCombination.length = 0;
+      playersKeys = [];
+      difficulty = 1;
+      showColors(false);
     },
     view: (vnode) => {
       return [
         m('.ui.large.header',{ style: { marginTop: '10px', textAlign: 'center'} },  'Simon Says'),
+        m('.ui.container', [
+          m('.ui.button', { onclick: () => playersKeys.length = 0 }, 'RESET'),
+          m('.ui.label', `Colores Pulsados: ${playersKeys.toString()}`),
+        ]),
+        m('.ui.container', { style: { 'margin-top': '19px' } }, [
+          m('.ui.circular.icon.button', { 
+            style: { 'font-size': '19' },
+            onclick: () => difficulty < 4 ? difficulty++ : null
+          }, '+'),
+          m('.ui.circular.icon.button', { 
+            style: { 'font-size': '19' },
+            onclick: () => difficulty > 1 ? difficulty-- : null
+          }, '-')
+        ], `DIFICULTAD ${difficulty}`),
         m('.ui.divider'),
-        playing ? m(SimonsCombination, { playing:playing }): null,
-
-        // * *Si el jugador pulsa 4 botones llama al componente que muestra su combinación
-
-        playersCombination.length === 4 ? [
-          m(PlayersCombination, { playing : playing, playersCombination }),
-          m(Result, { playing: playing })
-        ] : null,
+        playing ? m(SimonsOrder, { playing:playing, playersKeys, difficulty }): null,
 
         // * *Tablero de Simon
         m('svg', { width: '700px', height: '800px', viewBox: '0, 0, 700, 800', baseProfile: 'full' }, [
@@ -107,21 +98,15 @@ const Simon = function(){
           // Vertical abajo
           m('line', { x1:'350', y1: '420', x2: '350', y2: '650', stroke: 'black', 'stroke-width': '10' }),
 
-          // ?LETRAS
-          // m('text', {x:"200",y:"250",'font-size':"50",'text-anchor':"middle",fill:"black"},  'A'),
-          // m('text', {x:"450",y:"250",'font-size':"50",'text-anchor':"middle",fill:"black"},  'X'),
-          // m('text', {x:"200",y:"480",'font-size':"50",'text-anchor':"middle",fill:"black"},  'E'),
-          // m('text', {x:"450",y:"480",'font-size':"50",'text-anchor':"middle",fill:"black"},  'S'),
-
           // ?BOTONES
 
           // Amarillo
           m('path.a', { 
             d: 'M 290 332 H 50 C 50 332, 70 80, 350 50 V 280 C 350 280, 300 300, 290 332', 
-            fill: showSecrets ? 'yellow' : baseColor, 
+            fill: showColors ? 'yellow' : baseColor, 
             stroke: 'black', 'stroke-width': '10', 
             onclick: (e) => {
-              playersCombination.push('amarillo');
+              addColorToArray('amarillo');
               e.target.style.fill = 'yellow';
               setTimeout(()=> e.target.style.fill = baseColor, secretShowTimeout)
             }
@@ -129,10 +114,10 @@ const Simon = function(){
           // Rojo
           m('path', { 
             d: 'M 420 332 H 650 C 650 332, 650 80, 350 50 V 280 C 350 280, 400 300, 420 332', 
-            fill: showSecrets ? 'red' : baseColor, 
+            fill: showColors ? 'red' : baseColor, 
             stroke: 'black', 'stroke-width': '10',
             onclick: (e) => {
-              playersCombination.push('rojo');
+              addColorToArray('rojo');
               e.target.style.fill = 'red';
               setTimeout(() => e.target.style.fill = baseColor, secretShowTimeout);
             }
@@ -140,10 +125,10 @@ const Simon = function(){
           // Azul
           m('path', {
             d: 'M 290 332 H 50 C 50 332, 40 620, 350 650 V 420 C 332 420, 280 400, 290 332',
-            fill: showSecrets ? 'blue' : baseColor, 
+            fill: showColors ? 'blue': baseColor, 
             stroke: 'black', 'stroke-width': '10',
             onclick: (e) => {
-              playersCombination.push('azul');
+              addColorToArray('azul');
               e.target.style.fill = 'blue';
               setTimeout(() => e.target.style.fill = baseColor, secretShowTimeout)
             }
@@ -151,10 +136,10 @@ const Simon = function(){
           // Verde
           m('path', {
             d: 'M 420 332 H 650 C 650 332, 650 650 , 350 650 V 420 C 350 420, 400 420, 420 332',
-            fill: showSecrets ? 'green' : baseColor, 
+            fill: showColors ? 'green': baseColor,
             stroke: 'black', 'stroke-width': '10',
             onclick: (e) => {
-              playersCombination.push('verde');
+              addColorToArray('verde');
               e.target.style.fill = 'green';
               setTimeout(() => e.target.style.fill = baseColor, secretShowTimeout)
             }
@@ -165,16 +150,15 @@ const Simon = function(){
            { cx: '50%', cy: '350',r: '70',stroke: 'black', 'stroke-width': "17", fill: 'orange',
             onclick: () => { 
               playing = !playing;
-              // * *Si pulsa para terminar la partida, resetea la combinación del jugador a vacio.
-                playersCombination.length = 0;
-                combinations.player = '';
+              showColors(true);
+              setTimeout(showColors(false), 3000);
             } 
            },
           ),
           m('text', {x:"50%",y:"355",'font-size':"30",'text-anchor':"middle",fill:"black"},  playing ? 'Terminar' : 'Jugar'),
         ]),
       ]
-    }
+    },
   }
 }
 
